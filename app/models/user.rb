432fbb474a1dd36a601f98f1c6a271b5654c137a
xamimran <xamimran8991@gmail.com>
2022-10-25 16:07:19 -0700

    set defualt value to user type

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
   enum isadmin: [:admin, :standard]
+  before_save :set_user_type
+  private
+    def set_user_type
+      self.isadmin = 1
+    end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  # after_initialize :set_user_type
-  # private
-  #   def set_user_type
-  #     self.isadmin = 1
-  #   end
 end
