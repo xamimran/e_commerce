331ab2117b70fced3aca03fbb196d9d9f3f18e36
xamimran <xamimran8991@gmail.com>
2022-10-25 15:06:54 -0700

    Add cret for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,7 +39,6 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
-  config.hosts << "ecommerce-production-83db.up.railway.app"
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
