ae23b6270744e9842de729957dc0831ed3f66cd2
xamimran <xamimran8991@gmail.com>
2022-10-25 08:53:51 -0700

    Change Database to postgress

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_10_25_122708) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.string "description"
