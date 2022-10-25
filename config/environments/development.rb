8760a89207c566ea3bede8b94019c8e8e6aac4f1
xamimran <xamimran8991@gmail.com>
2022-10-25 12:26:51 -0700

    Add link to production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,7 +10,7 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
-
+  config.hosts << "ecommerce-production-83db.up.railway.app"
   # Show full error reports.
   config.consider_all_requests_local = true
 
